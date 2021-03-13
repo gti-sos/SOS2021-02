ffe7e4c126627b87c68751edee008db584931509
marinantonioj <antonio_josemr@hotmail.com>
2021-03-13 13:41:58 +0000

    server ready

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 
 var cool = require("cool-ascii-faces");
+var express = require("express");
+var app = express();
+var port = 10000;
 
-console.log(cool());
+app.get("/cool", (request,response) => {//request son los datos que me mandan a mi y response es para devolver datos al cliente
+	response.send(cool());
+	console.log("New request to /cool has arrived");
+});
+
+app.listen(port, () => {//la segunda parte del listen se ejecuta cuando el servidor esta listo
+	console.log("Server ready. Listening on port " + port);
+});
