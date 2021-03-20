cc01b09925ad8b3a54c611965afba998611f2a5f
Alejandro Fernandez <alefernandez1998af@gmai.com>
2021-03-20 21:11:20 +0100

    Añadido datos a la tabla

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ app.get("/info/oil-production-stats", (request,response) => {
 });
 
 //AFB
-app.get("/info/nuts-production-stats", (request,response) => {
-	response.send("<html><body><h1>En esta tabla se muestran los datos de la producción de almendras, nueces y pistachos en los diferentes países que más rendimiento sacan de ello</h1><table border><tr><th>country</th><th>year</th><th>almonds-prods</th><th>walnut-prods</th><th>pistachio-prods</th></tr><tr><th>Spain</th><th>2011</th><th>208800</th><th>13815</th><th>2708</th></tr><tr><th>Italy</th><th>2011</th><th>104790</th><th>10500</th><th>3079</th></tr><tr><th>Greece</th><th>2011</th><th>29800</th><th>29800</th><th>7791</th></tr><tr><th>Turkey</th><th>2011</th><th>69838</th><th>203212</th><th>112000</th></tr><tr><th>USA</th><th>2011</th><th>1655000</th><th>418212</th><th>201395</th></tr></table></body></html>");
-	console.log("New request to /info/nuts-production-stats has arrived");
+app.get("/info/wine-production-stats", (request,response) => {
+	response.send("<html><body><h1>Producción de vino a nivel mundial</h1><table border><tr><th>country</th><th>year</th><th>productions-wine</th><th>wine-imports</th><th>wine-exports</th></tr><tr><th>Spain</th><th>2011</th><th>33397</th><th>703</th><th>22430</th></tr><tr><th>Italy</th><th>2011</th><th>42772</th><th>2412</th><th>23238</th></tr><tr><th>Greece</th><th>2011</th><th>9132</th><th>203</th><th>376</th></tr><tr><th>Turkey</th><th>2011</th><th>596</th><th>80</th><th>25</th></tr><tr><th>USA</th><th>2011</th><th>19187</th><th>10155</th><th>4210</th></tr></table></body></html>");
+	console.log("New request to /info/wine-production-stats has arrived");
 });
 
 app.listen(port, () => {//la segunda parte del listen se ejecuta cuando el servidor esta listo
