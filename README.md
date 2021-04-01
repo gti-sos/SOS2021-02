925eabc3aa0313c4ba36ec8e70a09593aad932a1
Alejandro Fernandez <alefernandez1998af@gmai.com>
2021-04-01 19:52:21 +0200

    Codigo afb completado hasta post

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# SOS2021-02
+<html>
+	<head>
+		<title>Web Server</title>
+	</head>
+	<body>
+		<h1>SOS2021-02</h1>
+		<h3>Team</h3>
+		<ul>
+        <li><h3> <a href="https://github.com/marinantonioj">Antonio José Marín Rodríguez</a></h3></li>
+        <li><h3> <a href="https://github.com/jgonzalezal37"> José Manuel González Domínguez</a></h3></li>
+        <li><h3> <a href="https://github.com/alefdz98"> Alejandro Fernández Baena</a></h3></li>
+		</ul>
+   	    <h3>Project description:</h3>
+		<p>Nuestras fuentes de información están orientadas a comparar la producción de aceite, vino y frutos secos en los diferentes países que más rendimiento sacan de ellos.</p>
+	    <h3>Repository:</h3>
+	    <a href="https://github.com/gti-sos/SOS2021-02">gti-sos/SOS2021-02</a>
+		<h3>URL:</h3>
+		<a href="http://sos2021-02.herokuapp.com/">http://sos2021-02.herokuapp.com</a>
+		<h3>APIs:</h3>
+		<ul>
+        <li>
+			<a href="https://sos2021-00.herokuapp.com/api/v1/my-awesome-resource-1/docs">	  
+				[https://sos2021-00.herokuapp.com/api/v1/my-awesome-resource-1/docs]</a>
+				<a> Fuente </a>(developed by <a href="https://github.com/marinantonioj">Antonio José Marín Rodríguez)	</a>
+		</li>
+		<li>
+			<a href="https://sos2021-00.herokuapp.com/api/v1/my-awesome-resource-2/docs">      
+				[https://sos2021-00.herokuapp.com/api/v1/my-awesome-resource-2/docs]</a>
+				<a> Fuente </a>(developed by <a href="https://github.com/marinantonioj">José Manuel González Domínguez)	</a>
+		</li>
+		 <li>
+			<a href="https://sos2021-00.herokuapp.com/api/v1/my-awesome-resource-3/docs">   
+				[https://sos2021-00.herokuapp.com/api/v1/my-awesome-resource-3/docs]</a>
+				<a> Fuente </a>(developed by <a href="https://github.com/marinantonioj">Alejandro Fernandez Baena)	</a>
+		</li>
+	</body>
+</html>
--- index.js
+++ index.js
@@ -166,12 +166,86 @@ app.get("/info/oil-production-stats", (request,response) => {
 
 
 //=========================================== AFB =========================================================
+
+
+
+var winestats = [
+    {
+        "country": "Spain",
+        "year": 2011,
+		"production-wine": 33397,
+		"wine-import": 703,
+		"wine-export": 22430
+    },
+	{
+        "country": "Italy",
+        "year": 2011,
+		"production-wine": 42772,
+		"wine-import": 2412,
+		"wine-export": 23238
+    },
+	{
+		"country": "Greece",
+        "year": 2011,
+		"production-wine": 9132,
+		"wine-import": 203,
+		"wine-export": 376
+    },
+	{
+		"country": "Turkey",
+        "year": 2011,
+		"production-wine": 596,
+		"wine-import": 80,
+		"wine-export": 25
+    },
+	{
+        "country": "USA",
+        "year": 2011,
+		"production-wine": 19187,
+		"wine-import": 10155,
+		"wine-export": 4210
+    }
+];
+//Get al info (tabla)
 app.get("/info/wine-production-stats", (request,response) => {
-	response.send("<html><body><h1>Producción de vino a nivel mundial</h1><table border><tr><th>country</th><th>year</th><th>productions-wine</th><th>wine-imports</th><th>wine-exports</th></tr><tr><th>Spain</th><th>2011</th><th>33397</th><th>703</th><th>22430</th></tr><tr><th>Italy</th><th>2011</th><th>42772</th><th>2412</th><th>23238</th></tr><tr><th>Greece</th><th>2011</th><th>9132</th><th>203</th><th>376</th></tr><tr><th>Turkey</th><th>2011</th><th>596</th><th>80</th><th>25</th></tr><tr><th>USA</th><th>2011</th><th>19187</th><th>10155</th><th>4210</th></tr></table></body></html>");
+	response.send("<html><body><h1>Producción de vino a nivel mundial</h1><table border><tr><th>country</th><th>year</th><th>productions-wine</th><th>wine-import</th><th>wine-export</th></tr><tr><th>Spain</th><th>2011</th><th>33397</th><th>703</th><th>22430</th></tr><tr><th>Italy</th><th>2011</th><th>42772</th><th>2412</th><th>23238</th></tr><tr><th>Greece</th><th>2011</th><th>9132</th><th>203</th><th>376</th></tr><tr><th>Turkey</th><th>2011</th><th>596</th><th>80</th><th>25</th></tr><tr><th>USA</th><th>2011</th><th>19187</th><th>10155</th><th>4210</th></tr></table></body></html>");
 	console.log("New request to /info/wine-production-stats has arrived");
 });
 
+//GET loadInitialData
+app.get(BASE_API_PATH+"wine-production-stats/loadInitialData", (req, res) =>{
+    res.send(JSON.stringify(winestats, null, 2));
+	
+});
+
+//GET a toda la lista de recursos
+app.get(BASE_API_PATH+"wine-production-stats", (req, res) =>{
+    res.send(JSON.stringify(winestats, null, 2));
+});
+
+//GET a un recurso
+app.get(BASE_API_PATH+"wine-production-stats/:country/:year", (req, res) =>{
+	var reqcountry = req.params.country;
+	var reqyear = req.params.year;
+	var sendData = [];
+	for(var i=0; i<winestats.length-1; i++) {
+		if((String(winestats[i].country) === reqcountry) && (winestats[i].year === parseInt(reqyear))){
+			sendData.push(winestats[i]);
+		}
+    	res.send(JSON.stringify(sendData, null, 2));
+	}
+});
+
+//POST para crear un nuevo recurso en nuestra lista
+app.post(BASE_API_PATH+"wine-production-stats", (req, res) =>{
+    var newCountry = req.body;
+    console.log(`new country to be added:	<${JSON.stringify(newCountry,null,2)}>`);
+	winestats.push(newCountry);
+	res.sendStatus(201);
+});
+// =======================================Codigo de grupo===========================
 app.listen(port, () => {//la segunda parte del listen se ejecuta cuando el servidor esta listo
 	console.log("Server ready. Listening on port " + port);
 });
+
 
