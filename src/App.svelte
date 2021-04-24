6e5e05928b2cb49a8d2066ecb8f20ee2a266077a
Jose Manuel <jgonzalezal37@gmail.com>
2021-04-24 19:34:03 +0200

    empezando con svelte

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import OilTable from './OilTable.svelte'
+	const routes = {
+		
+		"/oil-production-stats": OilTable};
+	import {
+    Button,
+    
+  } from "sveltestrap";
+
+
+	
+	
+</script>
+
+<main>
+	<h1>Hello!</h1>
+	<a href="old_index_static.html"> View the components info</a>
+	<p>API de oilstats</p>
+	<a href="/oil-production-stats">Ver datos de oiltable</a>
+	
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	p {
+		color: #0c0c0c;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 500;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/OilTable.svelte
@@ -0,0 +1,52 @@
+<script>
+    import {
+        onMount
+    } from "svelte";
+
+    import Table from "sveltestrap/src/Table.svelte";
+ 
+    let oilstats = [];
+
+    async function getData(){
+        console.log("Fetching oilstats...");
+        const res = await fetch("/api/v1/oil-production-stats");
+
+        if(res.ok){
+            console.log("Ok.");
+            const json = await res.json();
+            oilstats = json;
+            console.log(`We have received ${oilstats.length} countries.`);
+        }else{
+            console.log("Error!");
+        }
+    }   
+    
+    onMount(getData);
+</script>
+
+<main>
+    <Table bordered>
+        <thead>
+            <tr>
+                <td>country</td>
+                <td>year</td>
+                <td>production</td>
+                <td>exportation</td>
+                <td>distribution</td>
+                
+            </tr>
+        </thead>
+        <tbody>
+            {#each oilstats as data}
+                <tr>
+                    <td>{data.country}</td>
+                    <td>{data.year}</td>
+                    <td>{data["production"]}</td>
+                    <td>{data["exportation"]}</td>
+                    <td>{data["distribution"]}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </Table>
+</main>
+
