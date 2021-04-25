a524eae810ac50f88aecc67878d71966b424c20d
Jose Manuel <jgonzalezal37@gmail.com>
2021-04-25 11:58:09 +0200

    doc postman incluida

--- src/App.svelte
+++ src/App.svelte
@@ -17,17 +17,24 @@
 
 <main>
 	<h1>SOS2021-02</h1>
-	<a href="old_index_static.html"> View the components info</a>
+	<a href="old_index_static.html"> Ver la información de los componentes del grupo</a>
 
 		<p>API de oilstats</p>
 		<a href="/#/oil-production-stats"
-            ><Button color="primary">oilAPI</Button></a
+            ><Button color="primary">Datos aceite</Button></a
+          >
+		  <a href="https://documenter.getpostman.com/view/10673756/TzJoDfUb"
+            ><Button color="primary">Documentación de Postman</Button></a
           >
 
 		<p>API de winestats</p>
 		<a href="/#/wine-production-stats"
-            ><Button color="primary">wineAPI</Button></a
+            ><Button color="primary">Datos vino</Button></a
           >
+		  <a href="https://documenter.getpostman.com/view/14969253/TzJoDfre"
+		  ><Button color="primary">Documentación de Postman</Button></a
+		>
+
 		<Router {routes}> </Router>
 	
 </main>
