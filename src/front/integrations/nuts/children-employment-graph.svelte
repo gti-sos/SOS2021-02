98b22ef57d591aae04ab9fcd7299cbe00c442c62
marinantonioj <antonio_josemr@hotmail.com>
2021-05-26 16:21:39 +0200

    nut

--- src/front/integrations/nuts/children-employment-graph.svelte
+++ src/front/integrations/nuts/children-employment-graph.svelte
@@ -17,6 +17,26 @@
     var mapa = new Map();
 
 
+    require.config({
+            baseUrl: '/js',
+            paths: {
+                d3: "http://d3js.org/d3.v5.min"
+                }
+        });
+
+        require(["d3", "c3"], function(d3, c3) {
+        c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ]
+            }
+        });
+        });
+
+
     function filtraElementos(value, key, map) {
         if(value.length == 3){
             ejeX.push(key);
@@ -25,10 +45,11 @@
             wineGeneral.push(value[2]);
         }
     }
-    
+
     async function loadGraph(){  
 
         const nuts = await fetch("/api/v2/nuts-production-stats");
+        
         if(nuts.ok){
             nutsData = await nuts.json();
             console.log(`We have received ${nutsData.length} data points: `);
@@ -150,11 +171,12 @@
 </script>
 
 <svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/series-label.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+<!-- Load c3.css -->
+<link href="/path/to/c3.css" rel="stylesheet">
+
+<!-- Load d3.js and c3.js -->
+<script src="/path/to/d3.v5.min.js" charset="utf-8"></script>
+<script src="/path/to/c3.min.js"></script>
 </svelte:head>
 
 <main>
