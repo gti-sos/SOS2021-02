44806622c8e35d5faad80a4c5f929a9c4230bd9c
Jose Manuel <jgonzalezal37@gmail.com>
2021-06-05 12:20:25 +0200

    e2e y grafica obesity con canvas

--- src/front/integrations/oil/obesity-graph.svelte
+++ src/front/integrations/oil/obesity-graph.svelte
@@ -8,123 +8,130 @@
     import Button from "sveltestrap/src/Button.svelte";
     
     let obesityData = [];
-    let manPercent = [];
-    let womanPercent = [];
-    let totalPopulation = [];
+    //let manPercent = [];
+    //let womanPercent = [];
+    //let totalPopulation = [];
     let ejeX = [];
+    let country = [];
+    let cont = 0;
     var mapa = new Map();
     var URL = " http://sos2021-10.herokuapp.com/api/integration/obesity-stats"
 
 
-    function filtraElementos(value, key, map) {
+    /*function filtraElementos(value, key, map) {
         if(value){
             ejeX.push(key);
             manPercent.push(value[0]);
             womanPercent.push(value[1]);
             totalPopulation.push(value[2]);
         }
-    }
+    }*/
     
     async function loadGraph(){  
 
         const obesity = await fetch(URL);
-        if(obesity.ok){
             obesityData = await obesity.json();
             console.log(`We have received ${obesityData.length} data points: `);
             console.log(JSON.stringify(obesityData,null,2));
-            obesityData.forEach(data => {
-                if(mapa.has(data.country + "-" + data.year)){
-                    let aux = mapa.get(data.country + "-" + data.year);
-                    aux.push(data["man_percent"]);
-                    aux.push(data["woman_percent"]);
-                    aux.push(data["total_population"]);
-                    mapa.set(data.country + "-" + data.year, aux);
-                }else{
-                    
-                    console.log(data["man_percent"]);
-                    let aux = [];
-                    aux.push(data["man_percent"]);
-                    aux.push(data["woman_percent"]);
-                    aux.push(data["total_population"]);
-                    console.log(aux);
-                    mapa.set(data.country + "-" + data.year, aux);
-                }
-            });
-        }else{
-            console.log("Error loading obesity");
-        }
+
+            let countryYear = [];
+            let dataMan = [];
+            let dataWoman = [];
+            let dataTotal = [];
+            obesityData.forEach((i)=>{
+            let countryYearData = i.country +  "-" + i.year;
+            let manPercentData = parseFloat(i.man_percent);
+            let womanPercentData = parseFloat(i.woman_percent);
+            let totalPopulationData = parseFloat(i.total_population);
+            dataMan.push({y: manPercentData, label: countryYearData});
+            dataWoman.push({y: womanPercentData, label: countryYearData});
+            dataTotal.push({y: totalPopulationData, label: countryYearData});
+            
+            cont++;
+        });
+        console.log(countryYear);
+        console.log(dataMan);
+        console.log(dataWoman);
+        console.log(dataTotal);
         
         
 
         console.log(mapa);
 
-        mapa.forEach(filtraElementos);
+        
+        var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	title:{
+		text: "Olympic Medals of all Times (till 2016 Olympics)"
+	},
+	axisY: {
+		title: "Medals",
+		includeZero: true
+	},
+	legend: {
+		cursor:"pointer",
+		itemclick : toggleDataSeries
+	},
+	toolTip: {
+		shared: true,
+		content: toolTipFormatter
+	},
+	data: [{
+		type: "bar",
+		showInLegend: true,
+		name: "manPercent",
+		color: "gold",
+		dataPoints: dataMan
+	},
+	{
+		type: "bar",
+		showInLegend: true,
+		name: "womanPercent",
+		color: "silver",
+		dataPoints: dataWoman
+	},
+	{
+		type: "bar",
+		showInLegend: true,
+		name: "totalPopulation",
+		color: "#A57164",
+		dataPoints: dataTotal
+	}]
+});
+chart.render();
 
-        Highcharts.chart('container', {
-            chart:{
-                type: 'column'
-            },
-            title: {
-                text: 'Gráfico General'
-            },
-            yAxis: {
-                title: {
-                    text: 'Cantidad'
-                }
-            },
-            xAxis: {
-                title: {
-                    text: 'Año'
-                },
-                categories: ejeX
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
-            },
-            series: [{
-                name: 'man_percent',
-                data: manPercent
-            },
-            {
-                name: ' woman_percent',
-                data: womanPercent
-            },
-            {
-                name: 'total_population',
-                data: totalPopulation
-            }],
-            responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
-                    },
-                    chartOptions: {
-                        legend: {
-                            layout: 'horizontal',
-                            align: 'center',
-                            verticalAlign: 'bottom'
-                        }
-                    }
-                }]
-            }
-        });
+function toolTipFormatter(e) {
+	var str = "";
+	var total = 0 ;
+	var str3;
+	var str2 ;
+	for (var i = 0; i < e.entries.length; i++){
+		var str1 = "<span style= \"color:"+e.entries[i].dataSeries.color + "\">" + e.entries[i].dataSeries.name + "</span>: <strong>"+  e.entries[i].dataPoint.y + "</strong> <br/>" ;
+		total = e.entries[i].dataPoint.y + total;
+		str = str.concat(str1);
+	}
+	str2 = "<strong>" + e.entries[0].dataPoint.label + "</strong> <br/>";
+	str3 = "<span style = \"color:Tomato\">Total: </span><strong>" + total + "</strong><br/>";
+	return (str2.concat(str)).concat(str3);
+}
+
+function toggleDataSeries(e) {
+	if (typeof (e.dataSeries.visible) === "undefined" || e.dataSeries.visible) {
+		e.dataSeries.visible = false;
+	}
+	else {
+		e.dataSeries.visible = true;
+	}
+	chart.render();
+}
   }
 </script>
 
 <svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/series-label.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js" on:load="{loadGraph}"></script>
 </svelte:head>
 
 <main>
-    <figure class="highcharts-figure">
-        <div id="container"></div>
-        
-    </figure> 
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
     <Button outline color="secondary" on:click="{pop}">Atrás</Button> 
 </main>
