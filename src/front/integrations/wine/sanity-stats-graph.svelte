dcf6908e658e31d48cc9bad87d475b2f74b68450
Alejandro Fernandez <alefernandez1998af@gmai.com>
2021-06-05 13:54:29 +0200

    graficas arreglada

--- src/front/integrations/wine/sanity-stats-graph.svelte
+++ src/front/integrations/wine/sanity-stats-graph.svelte
@@ -1,125 +1,138 @@
 <script>
-    import {
-        onMount
-    } from "svelte";
-    import {
-        pop
-    } from "svelte-spa-router";
     import Button from "sveltestrap/src/Button.svelte";
-    
-    let sanityData = [];
-    let health_expenditure_in_percentage = [];
-    let doctor_per_1000_habitant= [];
-    let hospital_bed= [];
-    let ejeX = [];
-    var mapa = new Map();
-    var URL = "https://sanity-integration.herokuapp.com/sanity-stats";
-
-    function filtraElementos(value, key, map) {
-        if(value){
-            ejeX.push(key);
-            health_expenditure_in_percentage.push(value[0]);
-            doctor_per_1000_habitant.push(value[1]);
-            hospital_bed.push(value[2]);
-        }
-    }
-    
-    async function loadGraph(){  
-
-        const sanity = await fetch(URL);
-        if(sanity.ok){
-            sanityData = await sanity.json();
-            console.log(`We have received ${sanityData.length} data points: `);
-            console.log(JSON.stringify(sanityData,null,2));
-            sanityData.forEach(data => {
-                if(mapa.has(data.country + "-" + data.year)){
-                    let aux = mapa.get(data.country + "-" + data.year);
-                    aux.push(data["health_expenditure_in_percentage"]);
-                    aux.push(data["doctor_per_1000_habitant"]);
-                    aux.push(data["hospital_bed"]);
-                    mapa.set(data.country + "-" + data.year, aux);
-                }else{
-                    console.log(data["health_expenditure_in_percentage"]);
-                    let aux = [];
-                    aux.push(data["health_expenditure_in_percentage"]);
-                    aux.push(data["doctor_per_1000_habitant"]);
-                    aux.push(data["hospital_bed"]);
-                    console.log(aux);
-                    mapa.set(data.country + "-" + data.year, aux);
-                }
+    import { pop } from "svelte-spa-router";
+      let Data = [];
+      let CountryDateData = [];
+      let health = [];
+      let doctor = [];
+      let hospital = [];
+      const BASE_API_PATH = "/api/v2";
+      let schoolData = [];
+      let schoolChartCountryDateData = [];
+      let cyd = [];
+      
+      async function loadChart() {
+          console.log("Fetching data...");
+          const res = await fetch("https://sanity-integration.herokuapp.com/sanity-stats");
+          Data = await res.json();
+          if (res.ok) {
+              Data.forEach(stat => {
+              CountryDateData.push(stat.country+"/"+stat.year);
+              health.push(stat["health_expenditure_in_percentage"]);
+              doctor.push(stat["doctor_per_1000_habitant"]);
+              hospital.push(stat["hospital_bed"]);   
             });
-        }else{
-            console.log("Error loading sanity");
-        }
-        
-        console.log(mapa);
-
-        mapa.forEach(filtraElementos);
-
-        Highcharts.chart('container', {
-            chart:{
-                type: 'column'
-            },
+          }
+          
+          console.log("Sanity chart: " + Data);
+          
+          Highcharts.chart("container", {
+            chart: {
+              type: 'column',
+              options3d: {
+                  enabled: true,
+                  alpha: 15,
+                  beta: 20,
+                  depth: 50,
+                  viewDistance: 60
+              }
+          },
             title: {
-                text: 'Gráfico General'
+              text: "Número de camas en hospitales",
             },
             yAxis: {
-                title: {
-                    text: 'Cantidad'
-                }
+              title: {
+                text: "",
+              },
             },
             xAxis: {
-                title: {
-                    text: 'Año'
-                },
-                categories: ejeX
+              title: {
+                text: "",
+              },
+              categories: CountryDateData
             },
             legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
+              layout: "vertical",
+              align: "right",
+              verticalAlign: "middle",
             },
-            series: [{
-                name: 'health_expenditure_in_percentage',
-                data: health_expenditure_in_percentage
-            },
-            {
-                name: 'doctor_per_1000_habitant',
-                data: doctor_per_1000_habitant
-            },{
-                name: 'hospital_bed',
-                data: hospital_bed
-            }],
+            annotations: [
+              {
+                labels: [
+                  {
+                    point: "date",
+                    text: "",
+                  },
+                  {
+                    point: "min",
+                    text: "Min",
+                    backgroundColor: "gray",
+                  },
+                ],
+              },
+            ],
+           plotOtions: {
+              series: {
+                  depth: 30,
+                  colorByPoint: false
+              }
+          },
+            series: [
+              {
+                name: "Número de camas",
+                data: hospital,
+                colorByPoint: true
+              },
+              
+            ],
             responsive: {
-                rules: [{
-                    condition: {
-                        maxWidth: 500
+              rules: [
+                {
+                  condition: {
+                    maxWidth: 3000,
+                  },
+                  chartOptions: {
+                    legend: {
+                      layout: "horizontal",
+                      align: "center",
+                      verticalAlign: "bottom",
                     },
-                    chartOptions: {
-                        legend: {
-                            layout: 'horizontal',
-                            align: 'center',
-                            verticalAlign: 'bottom'
-                        }
-                    }
-                }]
-            }
-        });
-  }
-</script>
-
-<svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/series-label.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
-</svelte:head>
-
-<main>
-    <figure class="highcharts-figure">
-        <div id="container"></div>
-        
-    </figure> 
-    <Button outline color="secondary" on:click="{pop}">Atrás</Button> 
-</main>
+                  },
+                },
+              ],
+            },
+          });
+        }
+      </script>
+      <svelte:head>
+        <script src="https://code.highcharts.com/highcharts.js" on:load={loadChart}></script>
+        <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+      <script src="https://code.highcharts.com/modules/cylinder.js"></script>
+      <script src="https://code.highcharts.com/modules/exporting.js"></script>
+      <script src="https://code.highcharts.com/modules/export-data.js"></script>
+      <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+      </svelte:head>
+      
+      <main>
+          <div>
+              <h1 style="text-align: center;">Uso <strong>API Sanity Stats </strong></h1>
+            </div>
+          
+          <div>
+              <figure class="highcharts-figure">
+                <div id="container" />
+                <p style="text-align: center;" class="highcharts-description">
+                  Se muestra el número de camas en hospitales en varios países y años.
+                </p>
+              </figure>
+              <Button id="back" outline color="secondary" on:click="{pop}"> Atrás</Button>
+            </div>
+      </main>
+      
+      <style>
+        .highcharts-figure {
+            min-width: 320px; 
+            max-width: 1400px;
+            margin: 1em auto;
+        }
+      </style>
