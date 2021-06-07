ee2398a2f0e0d58b53705b5b080e111fcf725415
Alejandro Fernandez <alefernandez1998af@gmai.com>
2021-06-07 21:00:09 +0200

    e2e 3

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -11,7 +11,7 @@
         <p>Enlaces a vídeos</p>
         <a href="https://youtu.be/kVghUVBlYok"
                 ><Button color="primary">Video Oil-stats (José Manuel González)</Button></a>
-        <a href="https://youtu.be/OBIOF7v4do0"
+        <a href="https://youtu.be/f8e47S8V-HE"
                 ><Button color="primary">Video Wine-stats (Alejandro Fernández)</Button></a>
         <a href="https://youtu.be/BhJIrdYyVCg"
                 ><Button color="primary">Video Nuts-stats (Antonio José Marín)</Button></a>
